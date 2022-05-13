4456afa657aba20dbc5e74ebef52c6d02de83519
Jeff Adams <jeff.c.adams@outlook.com>
2022-05-12 21:19:21 -0400

    Add DTOs

---
+++ src/DinnerPlansCommon/TableModels.cs
@@ -0,0 +1,15 @@
+﻿namespace DinnerPlansCommon;
+public record Meal(Guid Id,
+                   string Name,
+                   string Catagories,
+                   string Seasons,
+                   string Recipe,
+                   int Rating,
+                   int Priority);
+
+public record Menu(DateTime Date,
+                   Guid MealId,
+                   Guid RemovedMealId);
+
+public record Option(string Seasons,
+                     string Catagories);
