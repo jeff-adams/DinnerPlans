945947fbce3efcf7efe6078c58bf8056dacdc82a
Jeff Adams <jeff.c.adams@outlook.com>
2022-05-18 21:40:07 -0400

    Add catagories api

---
+++ src/DinnerPlansAPI/DinnerPlansCatagories.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Azure;
+using Azure.Data.Tables;
+using DinnerPlansCommon;
+
+namespace DinnerPlansAPI
+{
+    public static class DinnerPlansCatagories
+    {
+        private const string catagoriesTableName = "catagories";
+        private const string catagoriesPartionKey = "catagory";
+
+        [FunctionName("GetCatagories")]
+        public static async Task<IActionResult> GetCatagories(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "catagories")] HttpRequest req,
+            [Table(catagoriesTableName, Connection = "DinnerPlansTableConnectionString")] TableClient catagoryTable,
+            ILogger log)
+        {
+            log.LogInformation($"Catagory | GET | All Catagories");
+            AsyncPageable<TableEntity> catagoryResults;
+            try
+            {
+                catagoryResults = catagoryTable.QueryAsync<TableEntity>(catagory => catagory.PartitionKey  == catagoriesPartionKey);
+            }
+            catch (RequestFailedException)
+            {
+                return new OkObjectResult(new JsonResult(new EmptyResult()));
+            }
+
+            List<string> catagories = await catagoryResults.Select(row => row.RowKey).ToListAsync();
+            return new OkObjectResult(catagories);
+        }
+    }
+}
